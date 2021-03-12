f7ee0ffe76462fc13f746b21d73857e36b1db116
Dmitriy <41430216+pshik@users.noreply.github.com>
2021-03-12 10:10:29 +0300

    Initial commit

---
+++ app/src/main/java/ru/pshik/focusstart/currencies/MainActivity.java
@@ -0,0 +1,14 @@
+package ru.pshik.focusstart.currencies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
